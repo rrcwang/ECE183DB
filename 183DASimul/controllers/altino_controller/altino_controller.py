f0a149a8383ae58cdc8a81b2ea37fdce1b984ebb
Timothy Lee <timl2415@yahoo.com>
2020-05-12 18:46:09 -0700

    Implmeneted Pure Pursuit

--- 183DASimul/controllers/altino_controller/altino_controller.py
+++ 183DASimul/controllers/altino_controller/altino_controller.py
@@ -8,6 +8,7 @@ from controller import Keyboard
 import altino as al
 import camera_detection as cd
 import numpy as np
+import pure_pursuit as pp
 import csv
 import cv2
 
@@ -25,7 +26,7 @@ def log_frisbee_data(time, state):
         writer.writerow([time, state.x, state.y, state.z, state.dx, state.dy, state.dz, state.roll, state.pitch, state.yaw, state.d_roll, state.d_pitch, state.d_yaw])
 
 # create the Robot instance.
-altino = al.Altino()
+alti = al.Altino()
 # clear logs, add column headers
 f = open('gps_log.csv', "w+")
 with open('gps_log.csv', 'a', newline='') as file:
@@ -40,54 +41,56 @@ with open('frisbee_log.csv', 'a', newline='') as file:
 f = open('range_log.csv', "w+")
 
 # get the time step of the current world.
-timestep = altino.timeStep
+timestep = alti.timeStep
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
 #  motor = robot.getMotor('motorname')
 #  ds = robot.getDistanceSensor('dsname')
 #  ds.enable(timestep)
-altino.enable_sensors()
-
+alti.enable_sensors()
 keyboard = Keyboard()
 keyboard.enable(10)
 steer = 0
 current_time = 0;
+path = pp.get_path()
+pos = alti.gps.getValues() # get initial position
+path.insert(0, [0, -4.2])
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
-while altino.step(timestep) != -1:
+while alti.step(timestep) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()    
 
-    gps_data      = altino.gps.getValues()
-    range_data    = altino.range_finder.getRangeImageArray()
-    frisbee_data  = cd.wb_detect(altino, [0, 1, 0])
+    gps_data      = alti.gps.getValues()
+    range_data    = alti.range_finder.getRangeImageArray()
+    frisbee_data  = cd.wb_detect(alti, [0, 1, 0])
     #altino.camera.getImage()
     #camera_status = altino.camera.saveImage('frame.png', 0)
     current_time += timestep
-    radius = float('inf')
+    #radius = float('inf')
     
-    key = keyboard.getKey()
-    if key == Keyboard.UP:
-        altino.set_speed(50)
-    elif key == Keyboard.DOWN:
-        altino.set_speed(-20)
-    elif key == Keyboard.LEFT:
-        radius = -0.3
-        altino.set_speed(0)
-    elif key == Keyboard.RIGHT:
-        radius = 0.5
-        altino.set_speed(20)
-    else:
-        altino.set_speed(0)
-    altino.set_steer(radius)
+    #key = keyboard.getKey()
+    #if key == Keyboard.UP:
+    #    alti.set_speed(50)
+    #elif key == Keyboard.DOWN:
+    #    alti.set_speed(-20)
+    #elif key == Keyboard.LEFT:
+    #    radius = -0.3
+    #    alti.set_speed(0)
+    #elif key == Keyboard.RIGHT:
+    #    radius = 0.5
+    #    alti.set_speed(20)
+    #else:
+    #   alti.set_speed(0)
+    #alti.set_steer(radius)
+    
+    # Pure Pursuit
+    pp.pp_update(alti, (gps_data[0], gps_data[2]), alti.get_bearing(), path)
+    
     # Process sensor data here.
     log_gps_data(current_time, gps_data)
-    if frisbee_data:
-        pos = frisbee_data.get_position()
-        frisbee_state = cd.get_frisbee_state(pos[0], pos[1], pos[2], 0, 0, 0)
-        log_frisbee_data(current_time, frisbee_state)
 
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
